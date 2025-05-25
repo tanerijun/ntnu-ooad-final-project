99c3285a659afaea0aca2e6f7691c566b386e9b1
tanerijun <tanerivince@gmail.com>
2025-05-26 00:08:42 +0800

    feat: cleanup

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -8,7 +8,6 @@ import dayjs from 'dayjs';
 import { config } from '@/config';
 import { Budget } from '@/components/dashboard/overview/budget';
 import { LatestOrders } from '@/components/dashboard/overview/latest-orders';
-import { LatestProducts } from '@/components/dashboard/overview/latest-products';
 import { Sales } from '@/components/dashboard/overview/sales';
 import { TasksProgress } from '@/components/dashboard/overview/tasks-progress';
 import { TotalCustomers } from '@/components/dashboard/overview/total-customers';
@@ -20,7 +19,6 @@ export const metadata = { title: `Statistics | ${config.site.name}` } satisfies 
 export default function Page(): React.JSX.Element {
   return (
     <Stack gap={3}>
-      <Typography variant="body1">TODO: Implement proper statistics</Typography>
       <Grid container spacing={3}>
         <Grid lg={3} sm={6} xs={12}>
           <Budget diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
@@ -45,43 +43,6 @@ export default function Page(): React.JSX.Element {
         </Grid>
         <Grid lg={4} md={6} xs={12}>
           <Traffic chartSeries={[63, 15, 22]} labels={['Desktop', 'Tablet', 'Phone']} sx={{ height: '100%' }} />
-        </Grid>
-        <Grid lg={4} md={6} xs={12}>
-          <LatestProducts
-            products={[
-              {
-                id: 'PRD-005',
-                name: 'Soja & Co. Eucalyptus',
-                image: '/assets/product-5.png',
-                updatedAt: dayjs().subtract(18, 'minutes').subtract(5, 'hour').toDate(),
-              },
-              {
-                id: 'PRD-004',
-                name: 'Necessaire Body Lotion',
-                image: '/assets/product-4.png',
-                updatedAt: dayjs().subtract(41, 'minutes').subtract(3, 'hour').toDate(),
-              },
-              {
-                id: 'PRD-003',
-                name: 'Ritual of Sakura',
-                image: '/assets/product-3.png',
-                updatedAt: dayjs().subtract(5, 'minutes').subtract(3, 'hour').toDate(),
-              },
-              {
-                id: 'PRD-002',
-                name: 'Lancome Rouge',
-                image: '/assets/product-2.png',
-                updatedAt: dayjs().subtract(23, 'minutes').subtract(2, 'hour').toDate(),
-              },
-              {
-                id: 'PRD-001',
-                name: 'Erbology Aloe Vera',
-                image: '/assets/product-1.png',
-                updatedAt: dayjs().subtract(10, 'minutes').toDate(),
-              },
-            ]}
-            sx={{ height: '100%' }}
-          />
         </Grid>
         <Grid lg={8} md={12} xs={12}>
           <LatestOrders
